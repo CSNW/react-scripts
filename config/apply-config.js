f44f5c293de482f110d9f4d2e3685f48accb94a8
Tim Hall <tim.hall.engr@gmail.com>
2019-03-07 09:12:50 -0500

    Add applyServerConfig

--- config/apply-config.js
+++ config/apply-config.js
@@ -3,20 +3,25 @@ const { existsSync } = require('fs');
 const uuid = require('uuid/v4');
 const sanitize = require('sanitize-filename');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const mri = require('mri');
 
 function applyPaths(paths) {
-  const config_path = resolve('csnw.config.js');
-  if (!existsSync(config_path)) return paths;
+  const { dir, outdir } = resolveArgs();
 
-  const config = withCRA(require(config_path));
-  return Object.assign({}, paths, config.cra);
+  return Object.assign({}, paths, {
+    src: join(dir, 'src'),
+    static_files: join(dir, 'static'),
+    appBuild: outdir,
+    appIndexJs: join(dir, 'src', 'index.js'),
+    appHtml: join(dir, 'static', 'index.html'),
+    appTypeDeclarations: join(dir, 'src', 'react-app-env.d.ts'),
+  });
 }
 
 function applyWebpackConfig(webpack_config) {
-  const config_path = resolve('csnw.config.js');
-  if (!existsSync(config_path)) return webpack_config;
+  const config = loadConfig();
+  if (!config) return webpack_config;
 
-  const config = withCRA(require(config_path));
   return config.webpack(webpack_config, {
     buildId: uuid(),
     dev: process.env.NODE_ENV !== 'production',
@@ -28,29 +33,39 @@ function applyWebpackConfig(webpack_config) {
   });
 }
 
-function withCRA(next_config = {}) {
-  const { src = 'src', public = 'public', pages, build = 'build', static } =
-    next_config.cra || {};
+function applyServerConfig(webpack_dev_server_config) {
+  const config = loadConfig();
+  if (!config || !config.server) return webpack_dev_server_config;
 
-  const appSrc = resolve(src);
-  const appPublic = resolve(public);
-  const appBuild = resolve(build);
-  const appIndexJs = join(appSrc, pages ? pages.index : 'index.js');
-  const appHtml = join(appPublic, 'index.html');
-  const appTypeDeclarations = join(appSrc, 'react-app-env.d.ts');
-  const appStatic = static && resolve(static);
+  return config.server(webpack_dev_server_config);
+}
+
+function loadConfig() {
+  const { dir } = resolveArgs();
+  const config_path = join(dir, 'csnw.config.js');
+  if (!existsSync(config_path)) return;
+
+  return withCRA({ ...require(config_path), dir });
+}
+
+function resolveArgs() {
+  const args = mri(process.argv.slice(2), {
+    alias: {
+      o: 'outdir'
+    }
+  });
+  const dir = resolve(args._[0] || '.');
+  const outdir = args.outdir ? resolve(args.outdir) : join(dir, 'build');
+
+  return { dir, outdir };
+}
+
+function withCRA(next_config = {}) {
+  const { dir = resolve('.') } = next_config;
+  const src = join(dir, 'src');
+  const static_files = join(dir, 'static');
 
   return Object.assign({}, next_config, {
-    cra: {
-      appSrc,
-      appPublic,
-      appBuild,
-      appIndexJs,
-      appHtml,
-      appTypeDeclarations,
-      appStatic,
-    },
-
     webpack(config, { dev }) {
       if (pages) {
         const entries = {};
@@ -59,13 +74,13 @@ function withCRA(next_config = {}) {
 
         for (let [target, entry] of Object.entries(pages)) {
           const name = sanitize(target);
-          target = join(appPublic, `${target}.html`);
+          target = join(static_files, `${target}.html`);
 
-          entries[name] = join(appSrc, entry);
+          entries[name] = join(src, entry);
           plugins.push(
             new HtmlWebpackPlugin({
               template: `${loader}!${target}`,
-              filename: relative(appPublic, target),
+              filename: relative(static_files, target),
               chunks: [name],
             })
           );
@@ -97,4 +112,4 @@ function withCRA(next_config = {}) {
   });
 }
 
-module.exports = { applyPaths, applyWebpackConfig };
+module.exports = { applyPaths, applyWebpackConfig, applyServerConfig };
